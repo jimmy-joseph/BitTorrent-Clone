588113c41e749e725af50ec65fcf6ad55ac9a71d
fho <fho2501@gmail.com>
2026-03-09 20:51:42 -0400

    Introduced a basic handshake system among peers

---
+++ peerConnection.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.net.*;
+
+public class peerConnection extends Thread {
+
+    Socket socket;
+    int remotePeerId;
+
+    DataInputStream in;
+    DataOutputStream out;
+
+    public peerConnection(Socket socket, int remotePeerId) throws Exception {
+
+        this.socket = socket;
+        this.remotePeerId = remotePeerId;
+
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+
+        sendHandshake();
+        receiveHandshake();
+    }
+
+    public void run() {
+
+        try {
+
+            while (true) {
+
+                int length = in.readInt();
+                byte type = in.readByte();
+
+                byte[] payload = new byte[length - 1];
+
+                if (payload.length > 0)
+                    in.readFully(payload);
+
+                handleMessage(type, payload);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void sendHandshake() throws Exception {
+
+        byte[] handshake = Handshake.create(peerProcess.peerId);
+
+        out.write(handshake);
+        out.flush();
+    }
+
+    void receiveHandshake() throws Exception {
+
+        byte[] handshake = new byte[32];
+
+        in.readFully(handshake);
+
+        int peerId = Handshake.extractPeerId(handshake);
+
+        System.out.println("Received handshake from peer " + peerId);
+    }
+
+    void handleMessage(byte type, byte[] payload) {
+
+        switch (type) {
+
+            case 0:
+                Logger.log("Received CHOKE from " + remotePeerId);
+                break;
+
+            case 1:
+                Logger.log("Received UNCHOKE from " + remotePeerId);
+                break;
+
+            case 2:
+                Logger.log("Received INTERESTED from " + remotePeerId);
+                break;
+
+            case 3:
+                Logger.log("Received NOT_INTERESTED from " + remotePeerId);
+                break;
+        }
+    }
+}
---
+++ peerProcess.java
@@ -0,0 +1,157 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class peerProcess {
+
+    static int peerId;
+    static Map<Integer, PeerInfo> peers = new LinkedHashMap<>();
+
+    static int numPreferredNeighbors;
+    static int unchokingInterval;
+    static int optimisticUnchokingInterval;
+    static String fileName;
+    static int fileSize;
+    static int pieceSize;
+
+    static ServerSocket serverSocket;
+
+    public static void main(String[] args) throws Exception {
+
+        if (args.length != 1) {
+            System.out.println("Usage: java peerProcess <peerId>");
+            return;
+        }
+
+        peerId = Integer.parseInt(args[0]);
+
+        readCommonConfig();
+        readPeerInfo();
+
+        Logger.init(peerId);
+
+        int port = peers.get(peerId).port;
+
+        serverSocket = new ServerSocket(port);
+
+        Logger.log("Peer " + peerId + " started on port " + port);
+
+        connectToPreviousPeers();
+
+        listenForConnections();
+    }
+
+    static void readCommonConfig() throws Exception {
+
+        BufferedReader br = new BufferedReader(new FileReader("Common.cfg"));
+
+        String line;
+
+        while ((line = br.readLine()) != null) {
+
+            String[] parts = line.split(" ");
+
+            switch (parts[0]) {
+
+                case "NumberOfPreferredNeighbors":
+                    numPreferredNeighbors = Integer.parseInt(parts[1]);
+                    break;
+
+                case "UnchokingInterval":
+                    unchokingInterval = Integer.parseInt(parts[1]);
+                    break;
+
+                case "OptimisticUnchokingInterval":
+                    optimisticUnchokingInterval = Integer.parseInt(parts[1]);
+                    break;
+
+                case "FileName":
+                    fileName = parts[1];
+                    break;
+
+                case "FileSize":
+                    fileSize = Integer.parseInt(parts[1]);
+                    break;
+
+                case "PieceSize":
+                    pieceSize = Integer.parseInt(parts[1]);
+                    break;
+            }
+        }
+
+        br.close();
+    }
+
+    static void readPeerInfo() throws Exception {
+
+        BufferedReader br = new BufferedReader(new FileReader("PeerInfo.cfg"));
+
+        String line;
+
+        while ((line = br.readLine()) != null) {
+
+            String[] parts = line.split(" ");
+
+            int id = Integer.parseInt(parts[0]);
+            String host = parts[1];
+            int port = Integer.parseInt(parts[2]);
+            int hasFile = Integer.parseInt(parts[3]);
+
+            peers.put(id, new PeerInfo(id, host, port, hasFile == 1));
+        }
+
+        br.close();
+    }
+
+    static void connectToPreviousPeers() throws Exception {
+
+        for (PeerInfo p : peers.values()) {
+
+            if (p.id == peerId)
+                break;
+
+            Socket socket = new Socket(p.host, p.port);
+
+            Logger.log("Peer " + peerId + " makes a connection to Peer " + p.id);
+
+            new peerConnection(socket, p.id).start();
+        }
+    }
+
+    static void listenForConnections() throws Exception {
+
+        while (true) {
+
+            Socket socket = serverSocket.accept();
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+
+            byte[] handshake = new byte[32];
+
+            in.readFully(handshake);
+
+            int remotePeerId = Handshake.extractPeerId(handshake);
+
+            Logger.log("Peer " + peerId + " is connected from Peer " + remotePeerId);
+
+            peerConnection connection = new peerConnection(socket, remotePeerId);
+
+            connection.start();
+        }
+    }
+}
+
+class PeerInfo {
+
+    int id;
+    String host;
+    int port;
+    boolean hasFile;
+
+    PeerInfo(int id, String host, int port, boolean hasFile) {
+        this.id = id;
+        this.host = host;
+        this.port = port;
+        this.hasFile = hasFile;
+    }
+}
