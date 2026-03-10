b6f665ed653f2f374148485512fe53f793b426ff
jimmy-joseph <jimmy@jimmyjoseph.com>
2026-03-09 22:02:20 -0400

    revert bitfield log

--- peerConnection.java
+++ peerConnection.java
@@ -81,10 +81,6 @@ public class peerConnection extends Thread {
             case 3:
                 Logger.log("Received NOT_INTERESTED from " + remotePeerId);
                 break;
-
-            case 5:
-                Logger.log("Received BITFIELD from " + remotePeerId);
-                break;
         }
     }
 }
